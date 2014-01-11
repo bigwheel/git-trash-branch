728b535042c8d75f2f0e5c609da00d651160ffee
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-12 04:37:13 +0900

    先ほどのを微修正しつつヘルプ表示のテストを追加した

--- test/git_trash_branch_spec.rb
+++ test/git_trash_branch_spec.rb
@@ -7,21 +7,25 @@ SUBJECT_COMMAND = "#{PROJECT_ROOT}/git-trash-branch"
 TEMP_DIRECTORY_FOR_TEST = File::expand_path('../temp_dir', __FILE__)
 
 describe 'git-trash-branch' do
-  #before :all do
-  #  `mkdir -p #{TEMP_DIRECTORY_FOR_TEST}`
-  #end
-
-  #it 'should return 1 if branch does\'nt exists' do
-  it 'does\'nt success when a branch name is not given (only show help text)' do
+  before :all do
     `mkdir -p #{TEMP_DIRECTORY_FOR_TEST}`
     Dir::chdir TEMP_DIRECTORY_FOR_TEST
     `git init`
     `echo 'test_text' > test_file`
     `git add test_file && git commit -m "first commit"`
+  end
 
+  after :all do
+    `rm -rf #{TEMP_DIRECTORY_FOR_TEST}`
+  end
+
+  it 'does\'nt success when a branch name was not given' do
     status, = systemu(SUBJECT_COMMAND)
     status.should_not be_success
+  end
 
-    `rm -rf #{TEMP_DIRECTORY_FOR_TEST}`
+  it 'success when -h option was given' do
+    status, = systemu("#{SUBJECT_COMMAND} -h")
+    status.should be_success
   end
 end
