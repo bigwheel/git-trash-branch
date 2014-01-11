8e3481ab7c7ef8bcc0bbbd419cfee78e19061efe
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-12 04:58:14 +0900

    いよいよ移動操作のテストを書き始めた

--- test/git_trash_branch_spec.rb
+++ test/git_trash_branch_spec.rb
@@ -7,6 +7,9 @@ SUBJECT_COMMAND = "#{PROJECT_ROOT}/git-trash-branch"
 TEMP_DIRECTORY_FOR_TEST = File::expand_path('../temp_dir', __FILE__)
 
 describe 'git-trash-branch' do
+  SRC_BRANCH_NAME = 'src_branch'
+  DST_BRANCH_NAME = 'trash/src_branch'
+
   before :all do
     `mkdir -p #{TEMP_DIRECTORY_FOR_TEST}`
     Dir::chdir TEMP_DIRECTORY_FOR_TEST
@@ -27,5 +30,22 @@ describe 'git-trash-branch' do
   it 'success when -h option was given' do
     status, = systemu("#{SUBJECT_COMMAND} -h")
     status.should be_success
+  end
+
+  it 'does\'nt success when a branch name didn\'t exist' do
+    status, = systemu("#{SUBJECT_COMMAND} #{SRC_BRANCH_NAME}")
+    status.should_not be_success
+  end
+
+  it 'success when a existing branch name was given' do
+    `git checkout #{SRC_BRANCH_NAME}`
+    status, = systemu("#{SUBJECT_COMMAND} #{SRC_BRANCH_NAME}")
+    status.should be_success
+  end
+
+  it 'does\'nt success when current brach was specified as to trash' do
+    `git checkout -b #{SRC_BRANCH_NAME}`
+    status, = systemu("#{SUBJECT_COMMAND} #{SRC_BRANCH_NAME}")
+    status.should_not be_success
   end
 end
