89ed775a772511a90879642c39a065d930c44bc6
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-12 05:25:36 +0900

    記述が間違っていた箇所を修正

--- test/git_trash_branch_spec.rb
+++ test/git_trash_branch_spec.rb
@@ -5,23 +5,23 @@ Bundler.require(:default)
 PROJECT_ROOT = File::expand_path('../../', __FILE__)
 SUBJECT_COMMAND = "#{PROJECT_ROOT}/git-trash-branch"
 TEMP_DIRECTORY_FOR_TEST = File::expand_path('../temp_dir', __FILE__)
+FIRST_PWD= Dir::pwd
 
 describe 'git-trash-branch' do
   SRC_BRANCH_NAME = 'src_branch'
   DST_BRANCH_NAME = 'trash/src_branch'
 
-  before :all do
+  before :each do
     `mkdir -p #{TEMP_DIRECTORY_FOR_TEST}`
-    BEFORE_PWD = Dir::pwd
     Dir::chdir TEMP_DIRECTORY_FOR_TEST
     `git init`
     `echo 'test_text' > test_file`
     `git add test_file && git commit -m "first commit"`
-    Dir::chdir BEFORE_PWD
   end
 
-  after :all do
+  after :each do
     `rm -rf #{TEMP_DIRECTORY_FOR_TEST}`
+    Dir::chdir FIRST_PWD
   end
 
   it 'does\'nt success when a branch name was not given' do
